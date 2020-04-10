7d4e6abf11a9c0ebcb14d4ab552543dc703cfa68
krsnaapoorv <apoorva15997@gmail.com>
2020-04-10 15:13:57 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
         </div>
         <div className="row col-md-8 col-sm-12 justify-content-center">
           <h5 className="text-center text-muted">Looking forward towards establishing a professional career in such an organisation where I may be able to apply the knowledge, I have acquired, to contribute to the organisational goals and learn new technologies in order to upgrade myself further.</h5>
-          <button className="btn btn-lg btn-outline-info"> Download Resume</button>
+          <a className="ml-3 text-dark" href="https://drive.google.com/file/d/1zN-ZbW7oobOc0IZ8jy0RxzUouS78E3mI/view?usp=sharing" target="blank"><button className="btn btn-lg btn-outline-info"> Download Resume</button></a>
         </div>
         <div className="col-md-2 col-sm-0">
 
@@ -52,43 +52,43 @@ function App() {
       </div>
       <div className="row ml-5  justify-content-center">
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/git-icon.svg" style={{width: "111px"}} />
+            <img  src="/git-icon.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-5 mt-3">GIT</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 ">
-            <img  src="/html-5.svg" style={{width: "111px"}} />
+            <img  src="/html-5.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4">HTML5</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-            <img  src="/css-3.svg" style={{width: "111px"}} />
+            <img  src="/css-3.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4">CSS</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/bootstrap.svg" style={{width: "111px"}} />
+            <img  src="/bootstrap.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-1 mt-3">BOOTSTRAP</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/javascript.svg" style={{width: "111px"}} />
+            <img  src="/javascript.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-2 mt-3">JAVASCRIPT</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/react.svg" style={{width: "111px"}} />
+            <img  src="/react.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4 mt-4">REACT</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/redux.svg" style={{width: "111px"}} />
+            <img  src="/redux.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4 mt-4">REDUX</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/python.svg" style={{width: "111px"}} />
+            <img  src="/python.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4 mt-4">PYTHON</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-4">
-            <img  src="/flask.svg" style={{width: "111px"}} />
+            <img  src="/flask.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4 mt-4">FLASK</p>
         </div>
         <div className="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 mt-5">
-            <img  src="/mysql.svg" style={{width: "111px"}} />
+            <img  src="/mysql.svg" style={{width: "90px"}} />
             <p className="mt-2 ml-4 mt-4">MYSQL</p>
         </div>
       </div>
@@ -100,28 +100,31 @@ function App() {
         <div className="col-md-3 mt-4">
             <img  src="/react.svg" style={{width: "111px"}} />
             <h5 className="mt-2 ml-4 mt-4 text-muted font-weight-bold">REACT APP</h5>
+            <p>When it comes to react apps<br></br>I have the strength of Atlas</p>
         </div>
         <div className="col-md-3 mt-3">
             <img  src="/database.png" style={{width: "111px"}} />
             <h5 className="mt-2 ml-4 mt-4 text-muted font-weight-bold">BACKEND</h5>
+            <p>I am the one who runs<br></br>the show</p>
         </div>
         <div className="col-md-3 mt-3">
             <img  src="/frontend.png" style={{width: "111px"}} />
             <h5 className="mt-2 ml-4 mt-4 text-muted font-weight-bold">FRONTEND</h5>
+            <p>Now just that I can<br></br>even host the show</p>
         </div>
         <div className="col-md-3 mt-3">
             <img  src="/datastructure.png" style={{width: "111px"}} />
             <h5 className="mt-2 ml-4 mt-4 text-muted font-weight-bold">DATA STRUCTURES</h5>
+            <p>Putting the right blocks in the<br></br>right place is just my thing</p>
         </div>
       </div>
       <div>
       <hr></hr>
       <div className="row justify-content-center">
-        <h1 className="text-muted  display-4">PROJECTS</h1>
-
+        <h1 className="text-muted  display-4">Projects</h1>
         <div className="container-fluid text-center mt-4 mb-5">
-            <div className="row">
-              <div className="col-lg-4 col-md-6 col-sm-12 mb-1">
+            <div className="row justify-content-center">
+              <div className="col-lg-4 col-md-6 col-sm-12 mb-5">
                   <div className="card  shadow-lg">
                     <div className="card-body ">
                       <img className="img-fluid mb-3" src="/expense.png" />
@@ -130,10 +133,10 @@ function App() {
                       <h5 class="card-text">
                         It is a expense manager which have different categories.You can add any expense with current date and time.It shows you the balance.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/krsnaapoorv/projects/tree/master/masai-sprint-2" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://manage-expense.netlify.com/" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
@@ -145,7 +148,7 @@ function App() {
                   </div>
               </div>
               <div className="col-lg-4 col-md-6 col-sm-12 mb-1">
-                  <div className="card  shadow-lg">
+                  <div className="card  shadow-lg pb-4">
                     <div className="card-body ">
                       <img className="img-fluid mb-3" src="/currency.png" />
                       <hr></hr>
@@ -153,14 +156,14 @@ function App() {
                       <h5 class="card-text">
                         It is a Currency Converter where you can check the current value of your currency with 160 different currencies.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/krsnaapoorv/projects/tree/master/masai-sprint-3" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://convert-currency-krsna.netlify.com/" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
-                    <div className="row justify-content-center pb-3 float-left">
+                    <div className="row justify-content-center pb-4 float-left">
                         <img style={{width: "40px"}} src="/html-5.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/bootstrap.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/javascript.svg" alt="" />
@@ -169,7 +172,7 @@ function App() {
                   </div>
               </div>
               <div className="col-lg-4 col-md-6 col-sm-12 mb-1">
-                  <div className="card  shadow-lg">
+                  <div className="card  shadow-lg pb-4">
                     <div className="card-body ">
                       <img className="img-fluid mb-3" src="/gitfinder.png" />
                       <hr></hr>
@@ -177,14 +180,14 @@ function App() {
                       <h5 class="card-text">
                         Here You can find different git users as well as the repositories.You can also check the scores of each git repository.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://finder-git.netlify.com/" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_09/day_2/session_1" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
-                    <div className="row justify-content-center pb-3 float-left">
+                    <div className="row justify-content-center pb-4 float-left">
                         <img style={{width: "40px"}} src="/html-5.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/bootstrap.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/react.svg" alt="" />
@@ -192,7 +195,7 @@ function App() {
                   </div>
               </div>
               <div className="col-lg-4 col-md-6 col-sm-12 mb-1">
-                  <div className="card  shadow-lg">
+                  <div className="card  shadow-lg pb-3">
                     <div className="card-body ">
                       <img className="img-fluid mb-3" src="/todo.png" />
                       <hr></hr>
@@ -200,14 +203,14 @@ function App() {
                       <h5 class="card-text">
                         A simple todo app where you can create, add , update , delete todo.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_10/day_2/session_1" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://todomaterial.netlify.com/" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
-                    <div className="row justify-content-center pb-3 mt-5 mt-4 float-left">
+                    <div className="row justify-content-center pb-4 mt-5 mt-4 float-left">
                         <img style={{width: "40px"}} src="/html-5.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/bootstrap.svg" alt="" />
                         <img style={{width: "40px"}} className="ml-2" src="/react.svg" alt="" />
@@ -216,7 +219,7 @@ function App() {
                   </div>
               </div>
               <div className="col-lg-4 col-md-6 col-sm-12 mb-1">
-                  <div className="card  shadow-lg">
+                  <div className="card  shadow-lg pb-1">
                     <div className="card-body ">
                       <img className="img-fluid mb-3" src="/stopwatch.png" />
                       <hr></hr>
@@ -224,10 +227,10 @@ function App() {
                       <h5 class="card-text">
                         A simple StopWatch and Timer.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_08/day_3/assignment" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://stopwatchtimer.netlify.com/" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
@@ -249,10 +252,10 @@ function App() {
                       <h5 class="card-text">
                         A clone of microsoft website where you can see different listed products and add it to your cart.
                       </h5>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_09/day_5" target="blank">
                         <i class="fab fa-github"></i><strong>Github</strong>
                       </a>
-                      <a class="card-link text-muted mt-5" href="#" target="blank">
+                      <a class="card-link text-muted mt-5" href="https://microsoft-dummy1.netlify.com/" target="blank">
                         <i class="fab fa-external-link-square-alt"></i><strong>Demo Link</strong>
                       </a>
                     </div>
@@ -263,6 +266,51 @@ function App() {
                     </div>
                   </div>
               </div>
+              <div className="col-lg-4 col-md-6 col-sm-12 mb-1 mt-5">
+                  <div className="card  shadow-lg pb-3">
+                    <div className="card-body ">
+                      <img className="img-fluid mb-3" src="/teachers.png" />
+                      <hr></hr>
+                      <h2 class="card-title font-weight-bold">Teachers Manager</h2>
+                      <h5 class="card-text">
+                        Here, you can manage teachers data based on of your institution based on the classes,subject and sections they are teching. 
+                      </h5>
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_23/day_5/evaluation" target="blank">
+                        <i class="fab fa-github"></i><strong>Github</strong>
+                      </a>
+                    </div>
+                    <div className="row justify-content-center pb-5  mt-5  float-left">
+                        <img style={{width: "40px"}} src="/html-5.svg" alt="" />
+                        <img style={{width: "40px"}} className="ml-2" src="/bootstrap.svg" alt="" />
+                        <img style={{width: "40px"}} className="ml-2" src="/react.svg" alt="" />
+                        <img style={{width: "50px"}} className="ml-2" src="/redux.svg" alt="" />
+                        <img style={{width: "50px"}} className="ml-2" src="/mysql.svg" alt="" />
+                    </div>
+                  </div>
+              </div>
+              <div className="col-lg-4 col-md-6 col-sm-12 mb-1 mt-5">
+                  <div className="card  shadow-lg pb-1">
+                    <div className="card-body ">
+                      <img className="img-fluid mb-3" src="/blog_app.png" />
+                      <hr></hr>
+                      <h2 class="card-title font-weight-bold">Blog Application</h2>
+                      <h5 class="card-text">
+                        A blog application where you can see blog written by different users and comment on that.You can also add,edit,delete your blog.
+                      </h5>
+                      <a class="card-link text-muted mt-5" href="https://github.com/masai-school/cohort_3/tree/master/submissions/sm_103_apoorva/week_18/day_4/sesssion_1" target="blank">
+                        <i class="fab fa-github"></i><strong>Github</strong>
+                      </a>
+                    </div>
+                    <div className="row justify-content-center mt-5 float-left">
+                        <img style={{width: "40px"}} src="/html-5.svg" alt="" />
+                        <img style={{width: "40px"}} className="ml-2" src="/bootstrap.svg" alt="" />
+                        <img style={{width: "40px"}} className="ml-2" src="/react.svg" alt="" />
+                        <img style={{width: "50px"}} className="ml-2" src="/redux.svg" alt="" />
+                        <img style={{width: "50px"}} className="ml-2" src="/mysql.svg" alt="" />
+                    </div>
+                    <br></br>
+                  </div>
+              </div>
             </div>
         </div>
       </div>
@@ -270,32 +318,32 @@ function App() {
       </div>
       <div className="row justify-content-center">
         <div>
-          <div className="circleDiv ml-5 mt-sm-2 d-flex justify-content-center ">
+          <div className="circleDiv ml-2 mt-sm-2 d-flex justify-content-center ">
             <h2 className=" text-center m-auto font-weight-bold">1200</h2>
           </div>
           <h4 className="text-center text-muted m-auto font-weight-bold p-4">Hours Of Coding</h4>
         </div>
         
         <div>
-          <div className="circleDiv ml-5 mt-sm-2 d-flex justify-content-center ">
-            <h2 className="text-center m-auto font-weight-bold">200+</h2>
+          <div className="circleDiv ml-4 mt-sm-2 d-flex justify-content-center ">
+            <h2 className="text-center m-auto font-weight-bold">240+</h2>
           </div>
           <h4 className="text-center text-muted m-auto font-weight-bold p-4">Hackerrank Problems</h4>
         </div>
         
         <div>
-          <div className="circleDiv ml-5 mt-sm-2 d-flex justify-content-center ">
-            <h2 className="text-center m-auto font-weight-bold">200</h2>
+          <div className="circleDiv ml-3 mt-sm-2 d-flex justify-content-center ">
+            <h2 className="text-center m-auto font-weight-bold">100</h2>
           </div>
           <h4 className="text-center text-muted m-auto font-weight-bold ml-5 p-4">Hours Of Soft Skill</h4>
         </div>
         
 
         <div>
-          <div className="circleDiv ml-5 mt-sm-2 d-flex justify-content-center ">
-            <h2 className="text-center m-auto font-weight-bold">245+</h2>
+          <div className="circleDiv ml-3 mt-sm-2 d-flex justify-content-center ">
+            <h2 className="text-center m-auto font-weight-bold">290+</h2>
           </div>
-          <h4 className="ml-4 text-center text-muted m-auto font-weight-bold p-4">Git Commits</h4>
+          <h4 className="text-center text-muted m-auto font-weight-bold p-4">Git Commits</h4>
         </div>
         
       </div>
